098d2fd8e10401852e3f07b8f5ffa329c67a2051
szab0lcs <szabolcskencse@gmail.com>
2020-06-13 16:07:54 +0300

    Change state with button

--- src/App.js
+++ src/App.js
@@ -10,12 +10,23 @@ class App extends Component {
       {name: 'Jack', age: 20}
     ]
   }
+  switchNameHandler = () => {
+    // console.log('Was clicked')
+    // THIS IS THE WRONG WAY TO USE --> this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+      {name: 'Maximilian', age: 28},
+      {name: 'John', age: 25},
+      {name: 'Jack', age: 22}
+    ]
+    })
+  }
   render() {
     return (
       <div className="App">
        <h1>It's a React app.</h1>
        <p>version 1.0</p>
-       <button>Switch name</button>
+       <button onClick={this.switchNameHandler}>Switch name</button>
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>

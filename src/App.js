ad943cf3c5e148185adf05adeb3abb59d474acdc
szab0lcs <szabolcskencse@gmail.com>
2020-06-13 15:16:53 +0300

    Functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //    <h1>It's a React app.</h1>
-    //   </div>
-    // );
-    return React.createElement( 'div',{className:'App'},React.createElement( 'h1', null,'Does this work?'))
+    return (
+      <div className="App">
+       <h1>It's a React app.</h1>
+       <p>version 1.0</p>
+       <Person></Person>
+      </div>
+    );
+    //return React.createElement( 'div',{className:'App'},React.createElement( 'h1', null,'Does this work?'))
   }
 }
 

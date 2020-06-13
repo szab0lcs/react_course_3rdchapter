657d7dad9e7424b0206c231b391e7ead655cbaa7
szab0lcs <szabolcskencse@gmail.com>
2020-06-13 14:58:49 +0300

    App_js_change

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-       <h1>It's a React app.</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //    <h1>It's a React app.</h1>
+    //   </div>
+    // );
+    return React.createElement( 'div',{className:'App'},React.createElement( 'h1', null,'Does this work?'))
   }
 }
 

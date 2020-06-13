2fecedaf4836643a23acd5af1a517d0c657e1569
szab0lcs <szabolcskencse@gmail.com>
2020-06-13 15:36:59 +0300

    Dynamic Content and Properties

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
        <h1>It's a React app.</h1>
        <p>version 1.0</p>
-       <Person />
-       <Person />
-       <Person />
+       <Person name="Max" age="28"/>
+       <Person name="John" age="25">My Hobbies: Racing</Person>
+       <Person name="Jack" age="20"/>
       </div>
     );
     //return React.createElement( 'div',{className:'App'},React.createElement( 'h1', null,'Does this work?'))
